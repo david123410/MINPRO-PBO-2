518a307f9012a911219f5d6382c79a1265e23ee2
david sebas s <144750420+david123410@users.noreply.github.com>
2024-10-13 15:23:04 +0800

    Add files via upload

---
+++ MINPRO1/src/main/java/com/mycompany/minpro1/CRUD.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.mycompany.minpro1;
+/**
+ *
+ * @author david
+ */
+public interface CRUD {
+    void create();
+    void read();
+    void update();
+    void delete();
+}
